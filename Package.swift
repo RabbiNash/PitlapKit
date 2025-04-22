4fab8f988f6292f2d3c8c38968668e0441d94b08
Tinashe Makuti <74738843+RabbiNash@users.noreply.github.com>
2025-04-22 22:42:05 +0100

    BUMP v0.2.3

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "PitlapKit",
-         url: "https://github.com/RabbiNash/PitlapKMM/releases/download/0.2.2/PitlapKit.xcframework.zip",
-         checksum:"0b0c2e4cbd3637df044e6477581c0216a79616c1508e583abec7bdf4a019cd92")
+         url: "https://github.com/RabbiNash/PitlapKMM/releases/download/0.2.3/PitlapKit.xcframework.zip",
+         checksum:"086f58ef1a7f92644fd561dfb719d99685d05959e6d8aa8a103f3b0ec2f4a02f")
    ]
 )
