64326749446648d1c30546edb7674a957482c6ff
Tinashe Makuti <74738843+RabbiNash@users.noreply.github.com>
2025-04-22 21:57:13 +0100

    Update Patch Version v0.2.2

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "PitlapKit",
-         url: "https://github.com/RabbiNash/PitlapKMM/releases/download/0.2.1/PitlapKit.xcframework.zip",
-         checksum:"a02721889dc129fcc710ddcc182816015010aa25b39e8cc0aa287603fc8bce20")
+         url: "https://github.com/RabbiNash/PitlapKMM/releases/download/0.2.2/PitlapKit.xcframework.zip",
+         checksum:"0b0c2e4cbd3637df044e6477581c0216a79616c1508e583abec7bdf4a019cd92")
    ]
 )
